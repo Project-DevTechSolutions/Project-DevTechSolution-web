53a6297709a4168a10d47b3ab1e38a00f5cf2f1b
Ashwin Parthiban <parthiban.4@osu.edu>
2024-02-26 17:51:01 -0500

    'message'

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,140 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+const openMenu = () => {
+  document.querySelector(".sidebar")?.classList.add("open");
 
+}
+const closeMenu = () => {
+  document.querySelector(".sidebar")?.classList.remove("open");
+  
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid-container">
+    <header className="header">
+        <div className="brand">
+        <button onClick={openMenu}>&#9776;</button>
+            <a href="index.html">Dev Tech Solutions</a>
+        </div>
+        <div className="header-links">
+            <a href="cart.html">Cart</a>
+            <a href="signin.html">Sign In</a>
+        </div>
+    </header>
+
+    <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+        <ul>
+            <li>
+                <a href="index.html">Pants</a>
+            </li>
+            <li>
+                <a href="index.html">Shirts</a>
+            </li>
+        </ul>
+   </aside>
+
+   <main>
+    <div className="content">
+        <ul className="products">
+            <li>
+                
+                <div className="products">
+                    <img className="product-image" src="images/d1.jpg" alt="product" />
+                    <ul>
+                    <li><div className="product-name">
+                        <a href="product.html">Slim Shirt</a>
+                    </div></li>
+                    <li> <div className="product-brand">Nike</div> </li>
+                    <li> <div className="product-price">$60</div> </li>
+                    <li> <div className="product-rating">4.5 Stars (10 Reviews)</div> </li>
+
+                   </ul>
+                </div>
+               
+            </li>
+            <li>
+                <div className="products">
+                    <img className="product-image" src="images/d1.jpg" alt="product" />
+                    <ul>
+                    <li><div className="product-name">
+                        <a href="product.html">Slim Shirt</a>
+                    </div></li>
+                    <li> <div className="product-brand">Nike</div> </li>
+                    <li> <div className="product-price">$60</div> </li>
+                    <li> <div className="product-rating">4.5 Stars (10 Reviews)</div> </li>
+
+                   </ul>
+                </div>
+            </li>
+            <li>
+                <div className="products">
+                    <img className="product-image" src="images/d1.jpg" alt="product" />
+                    <ul>
+                    <li><div className="product-name">
+                        <a href="product.html">Slim Shirt</a>
+                    </div></li>
+                    <li> <div className="product-brand">Nike</div> </li>
+                    <li> <div className="product-price">$60</div> </li>
+                    <li> <div className="product-rating">4.5 Stars (10 Reviews)</div> </li>
+
+                   </ul>
+                </div>
+            </li>
+            <li>
+                <div className="products">
+                    <img className="product-image" src="images/d1.jpg" alt="product" />
+                    <ul>
+                    <li><div className="product-name">
+                        <a href="product.html">Slim Shirt</a>
+                    </div></li>
+                    <li> <div className="product-brand">Nike</div> </li>
+                    <li> <div className="product-price">$60</div> </li>
+                    <li> <div className="product-rating">4.5 Stars (10 Reviews)</div> </li>
+
+                   </ul>
+                </div>
+            </li>
+            <li>
+                <div className="products">
+                    <img className="product-image" src="images/d1.jpg" alt="product" />
+                    <ul>
+                    <li><div className="product-name">
+                        <a href="product.html">Slim Shirt</a>
+                    </div></li>
+                    <li> <div className="product-brand">Nike</div> </li>
+                    <li> <div className="product-price">$60</div> </li>
+                    <li> <div className="product-rating">4.5 Stars (10 Reviews)</div> </li>
+
+                   </ul>
+                </div>
+            </li>
+            <li>
+                <div className="products">
+                    <img className="product-image" src="images/d1.jpg" alt="product" />
+                    <ul>
+                    <li><div className="product-name">
+                        <a href="product.html">Slim Shirt</a>
+                    </div></li>
+                    <li> <div className="product-brand">Nike</div> </li>
+                    <li> <div className="product-price">$60</div> </li>
+                    <li> <div className="product-rating">4.5 Stars (10 Reviews)</div> </li>
+
+                   </ul>
+                </div>
+            </li>
+        </ul>
+
     </div>
+   
+   </main>
+<footer> 
+    &copy; 2024 Dev Tech Solutions 
+</footer>
+    
+</div>
   );
 }
 
